d7b2bfda1745a24d9d43188acdf40b503e7e9edb
fudev1 <matteo.divita@outlook.be>
2023-08-09 14:31:12 +0200

    operateur

--- workspace/scripts/recuperation.js
+++ workspace/scripts/recuperation.js
@@ -5,6 +5,16 @@ db = db.getSiblingDB('technocite');
 
 // récupérer des données
 // équivalent du SELECT * FROM <collection>
-const classrooms = db.classrooms.find();
+// const classrooms = db.classrooms.find();
+
+
+// équivalent du SELECT * FROM classrooms WHERE code="Spirou"
+const classrooms = db.classrooms.find({
+    code : "Spirou", // commande classique
+    code : { $eq: "Spirou"}, // opérateur avec argument $eq = Là où le "code est égale à 'Spirou'"
+    capacity : { $gt: 10} // greater than
+});
+
+
 
 console.log(classrooms);
