6a8f6a32058915d465e292c2d56a5a6b5c477a6c
AmauryD <deflorenne.amaury@gmail.com>
2024-01-12 14:58:19 +0100

    insert

--- workspace/script.js
+++ workspace/script.js
@@ -1,6 +1,28 @@
 let db = connect("mongodb://root:test123@localhost");
+// équivalent du "use technocite"
+db = db.getSiblingDB("technocite");
 
-// liste les bases de données
-let dbList = db.adminCommand("listDatabases");
+// on va insérer dans "students" amaury
+const newStudent = db.students.insertOne({
+    name : "Amaury petit"
+});
 
-console.log(dbList);
+console.log(newStudent);
+
+const newStudents = db.students.insertMany([
+    {
+        name : "Amaury grand"
+    },
+    {
+        name : "Amaury moyen"
+    }
+]);
+
+// snake_case
+// camelCase
+// PascalCase
+// SCREAMINGCASE
+// kebab-case
+// Sponge
+
+console.log(newStudents);
