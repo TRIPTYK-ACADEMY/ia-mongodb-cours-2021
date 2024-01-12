1b7f5165c0d07d2dc747fbf2e42fcbc713d22dfa
AmauryD <deflorenne.amaury@gmail.com>
2024-01-12 12:06:29 +0100

    exec-script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ia-mongodb-cours-2021
+
+## SUR WINDOWS 
+
+./exec-script.bat
+
+## SUR LINUX
+
+bash exec-script.bash
--- workspace/script.js
+++ workspace/script.js
@@ -1,2 +1,6 @@
 let db = connect("mongodb://root:test123@localhost");
 
+// liste les bases de données
+let dbList = db.adminCommand("listDatabases");
+
+console.log(dbList);
