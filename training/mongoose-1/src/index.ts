22eaa6c4fa2eb8f5f0792ccad05858bbc1d609ba
AmauryD <deflorenne.amaury@gmail.com>
2024-01-16 11:51:25 +0100

    listing

--- training/mongoose-1/src/index.ts
+++ training/mongoose-1/src/index.ts
@@ -1,2 +1,13 @@
-// coucou
-console.log("Hello World!");
+import mongoose from "mongoose";
+import { ListingAndReviews } from "./schemas/listingsAndReviews";
+
+async function init() {
+    const mongo = await mongoose.connect('mongodb://root:test123@127.0.0.1:27017/sample_airbnb?authSource=admin');
+    console.log('Félicitations, vous vous êtes connecté à la DB', mongo.connection.db.databaseName);
+
+    const docs = await ListingAndReviews.find();
+
+    console.log(docs);
+}
+
+init();
